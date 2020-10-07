0fb4f3a2e63dba8721836b25b24608fe2edef7dd
hsthomp1 <hthompsonarizona@gmail.com>
2020-10-06 18:45:13 -0700

    AddingMachine Version 1.0
    Initial Commit:
    	 includes JavDoc comments

---
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+/**
+ * This program adds and subtracts numbers and returns them to the user in the form of an equation.
+ *  
+ * @author Hunter S. Thompson
+ * @version 1.0
+ *
+ */
+
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * Main method in which calls functions from the class. 
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Returns the current total.
+   * @return the total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Performs the add function in which the current value will be added to the current total.
+   * @param value number to be added to the total. 
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Performs the subtract function in which the current value will be subtracted from the current total.
+   * @param value number to be subtracted from the total.
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Outputs the equation with operations and values. 
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Clears the current equation/history.
+   */
+  public void clear() {
+  }
+}
